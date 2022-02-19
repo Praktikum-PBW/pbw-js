a40e5965cd49d603e3298fec3dd8adb744725112
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:18:36 +0700

    add file operator spread

---
+++ operator_spread.js
@@ -0,0 +1,6 @@
+let male = ["Ujang Sanjaya", "Kevin Alam", "Dedi Wijaya"];
+console.log(male);
+let female = ["Lilis Puspitasari", "Ririn Noviyanti", "Putri Wijaya"];
+console.log(female);
+let students = [...male, ...female];
+console.log(students);
