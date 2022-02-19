8c1f54ed39d4a2fb6368f43ba9408c7af1eab71a
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:17:16 +0700

    add file for-of

---
+++ for-of.js
@@ -0,0 +1,4 @@
+let faculties = ["Fakultas Ilmu Komputer", "Fakultas Hukum", "Fakultas Pertanian", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Teknik"];
+for (let faculty of faculties) {
+    console.log(faculty);
+}
