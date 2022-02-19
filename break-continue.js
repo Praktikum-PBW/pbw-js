b41537b86f833195e112d17085e0e0d9e4f24757
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:17:00 +0700

    add file break-continue

---
+++ break-continue.js
@@ -0,0 +1,16 @@
+// Break
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        break;
+    }
+    console.log(i);
+}
+
+// Continue
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        continue;
+    }
+    console.log(i);
+}
