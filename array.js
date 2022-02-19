e73cee85984aa06c66017aaea0a235f0422ac64d
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:16:51 +0700

    add file array

---
+++ array.js
@@ -0,0 +1,29 @@
+// Cara membuat array
+
+let faculties = ["Fakultas Ilmu Komputer", "Fakultas Hukum", "Fakultas Teknik", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Agama Islam"];
+
+// Cara mengambil elemen array
+console.log(faculties[0]);
+console.log(faculties[1]);
+console.log(faculties[2]);
+console.log(faculties[3]);
+console.log(faculties[4]);
+
+// cara mengubah elemen array
+faculties[1] = "Fakultas Ekonomi dan Bisnis";
+console.log(faculties);
+faculties[4] = 50;
+console.log(faculties);
+
+// array 2 dimensi
+let coordinate = [
+    [0, 3],
+    [6, 2],
+    [9, 8]
+];
+console.log(coordinate[0][0]);
+console.log(coordinate[0][1]);
+console.log(coordinate[1][0]);
+console.log(coordinate[1][1]);
+console.log(coordinate[2][0]);
+console.log(coordinate[2][1]);
