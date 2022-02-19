6fc2f5a6c9b5c21d85d65507e4fe945a9b8eac82
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:18:53 +0700

    add file switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+let grade = 'A';
+switch (grade) {
+    case 'A':
+        console.log("Very good");
+        break;
+    case 'B':
+        console.log("Good");
+        break;
+    case 'C':
+        console.log("Enough");
+        break;
+    case 'D':
+        console.log("Very Enough");
+        break;
+    default:
+        console.log("No Grade");
+        break;
+}
