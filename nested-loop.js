ca8ebc5105c91e20ca0063891ee345592b12aadb
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 15:17:54 +0700

    add file nested loop

---
+++ nested-loop.js
@@ -0,0 +1,8 @@
+let weeks = 3;
+let days = 7;
+for (let i = 1; i <= weeks; ++i) {
+    console.log("Week: " + i);
+    for (let j = 1; j <= days; ++j) {
+        console.log(" Day: " + j);
+    }
+}
